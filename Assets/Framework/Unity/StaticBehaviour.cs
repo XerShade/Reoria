cbfd81f36ba336474ca818f529494473d39abaf5
XerShade <jonathan.campbell.1992@hotmail.co.uk>
2021-01-14 13:59:52 -0500

    Added static behaviour class.

---
+++ Assets/Framework/Unity/StaticBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Reoria.Framework.Unity
+{
+    /// <summary>
+    /// Changes a GameObject to be static and loaded between scenes.
+    /// </summary>
+    public class StaticBehaviour : MonoBehaviour
+    {
+        /// <summary>
+        /// Checks to see if the object has already been set to DontDestroyOnLoad.
+        /// </summary>
+        [SerializeField]
+        private bool dontDestroyOnLoad;
+
+        /// <summary>
+        /// Called when the object is first created.
+        /// </summary>
+        void Awake()
+        {
+            // Check to see if DontDestroyOnLoad is set yet.
+            if (!dontDestroyOnLoad)
+            {
+                // Set the object to DontDestroyOnLoad so we can keep it between scenes.
+                DontDestroyOnLoad(gameObject);
+                dontDestroyOnLoad = true;
+
+                // Move it back to the _GameEngine scene, this is the only place static object should exist.
+                SceneManager.MoveGameObjectToScene(gameObject, SceneManager.GetSceneByName("_GameEngine"));
+            }
+        }
+    }
+}
