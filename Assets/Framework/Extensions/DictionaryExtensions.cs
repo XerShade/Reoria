e543ab2153d035aa730678d46fd2d43e25523634
XerShade <jonathan.campbell.1992@hotmail.co.uk>
2021-01-25 08:02:22 -0500

    Enhanced StaticBehaviour class.

---
+++ Assets/Framework/Extensions/DictionaryExtensions.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+
+namespace Reoria.Framework.Extensions
+{
+    /// <summary>
+    /// Defines extensions for the <see cref="Dictionary{TKey, TValue}"/> class.
+    /// </summary>
+    public static class DictionaryExtensions
+    {
+        public static TValue Add<TKey, TValue>(this Dictionary<TKey, TValue> dictionary, TKey key, TValue value, bool overwrite)
+        {
+            // Check to see if we already have an entry for this key.
+            if (!dictionary.ContainsKey(key))
+            {
+                // Lock the dictionary so other threads can't change it.
+                lock(dictionary)
+                {
+                    // Add the value to the dictionary.
+                    dictionary.Add(key, value);
+
+                    // Return the value we just added to the caller.
+                    return dictionary[key];
+                }
+            }
+            // Okay we already have one of these, check if they want to overwrite it.
+            else if (overwrite)
+            {
+                // Lock the dictionary so other threads can't change it.
+                lock (dictionary)
+                {
+                    // Remove the existing value from the dictionary.
+                    dictionary.Remove(key);
+
+                    // Add the value to the dictionary.
+                    dictionary.Add(key, value);
+
+                    // Return the value we just added to the caller.
+                    return dictionary[key];
+                }
+            }
+
+            // No operations were valid, return default to the caller.
+            return default;
+        }
+    }
+}
--- Assets/Framework/Unity/StaticBehaviour.cs
+++ Assets/Framework/Unity/StaticBehaviour.cs
@@ -1,17 +1,24 @@
 ﻿using System.Collections.Generic;
+using Reoria.Framework.Extensions;
 using UnityEngine;
 
 namespace Reoria.Framework.Unity
 {
     /// <summary>
-    /// Changes a GameObject to be static and loaded between scenes.
+    /// Changes a <see cref="UnityEngine.GameObject"/> to be static and remain loaded between scene changes.
     /// </summary>
     public class StaticBehaviour : MonoBehaviour
     {
         /// <summary>
-        /// A collection of names of all game objects currently loaded using this script.
+        /// A collection of all <see cref="UnityEngine.GameObject"/> instances currently loaded using this script.
         /// </summary>
-        private static readonly List<string> staticObjects = new List<string>();
+        private static readonly Dictionary<string, GameObject> staticObjects = new Dictionary<string, GameObject>();
+
+        /// <summary>
+        /// The <see cref="UnityEngine.GameObject"/> instace the script is currently managing.
+        /// </summary>
+        [SerializeField]
+        private GameObject reference = default;
 
         /// <summary>
         /// Called when the object is first created.
@@ -19,10 +26,10 @@ namespace Reoria.Framework.Unity
         void Awake()
         {
             // Check to see if the object has already been loaded.
-            if (!staticObjects.Contains(gameObject.name))
+            if (!staticObjects.ContainsKey(gameObject.name))
             {
-                // Add the object to the collection.
-                staticObjects.Add(gameObject.name);
+                // Add the object to the collection and store the reference.
+                reference = staticObjects.Add(gameObject.name, gameObject, false);
 
                 // Set the object to DontDestroyOnLoad so we can keep it between scenes.
                 DontDestroyOnLoad(gameObject);
@@ -35,16 +42,51 @@ namespace Reoria.Framework.Unity
         }
 
         /// <summary>
+        /// Called when the object is updated.
+        /// </summary>
+        private void Update()
+        {
+            // Check to see if the object has been loaded.
+            if (!staticObjects.ContainsKey(gameObject.name) || !staticObjects[gameObject.name].Equals(reference))
+            {
+                // We somehow lost the object reference, destroy it.
+                Destroy(gameObject);
+            }
+        }
+
+        /// <summary>
         /// Called when the object is destroyed.
         /// </summary>
         private void OnDestroy()
         {
             // Check to see if the object has been loaded.
-            if (!staticObjects.Contains(gameObject.name))
+            if (staticObjects.ContainsKey(gameObject.name))
             {
                 // Remove the object from the collection.
                 staticObjects.Remove(gameObject.name);
+
+                // Remove the stored reference to the object.
+                reference = null;
             }
         }
+    }
+
+    /// <summary>
+    /// Defines extensions for <see cref="UnityEngine.GameObject"/> instances to access their <see cref="StaticBehaviour"/> component.
+    /// </summary>
+    public static class StaticBehaviourExtensions
+    {
+        /// <summary>
+        /// Determines if the <see cref="UnityEngine.GameObject"/> is static and contains a <see cref="StaticBehaviour"/> component.
+        /// </summary>
+        /// <param name="gameObject">The <see cref="UnityEngine.GameObject"/> instance to run this function on.</param>
+        /// <returns>True if a <see cref="StaticBehaviour"/> component is found, false if not.</returns>
+        public static bool IsStatic(this GameObject gameObject) => gameObject.GetComponent<StaticBehaviour>() != null;
+        /// <summary>
+        /// Returns a static <see cref="UnityEngine.GameObject"/>'s <see cref="StaticBehaviour"/> component.
+        /// </summary>
+        /// <param name="gameObject">The <see cref="UnityEngine.GameObject"/> instance to run this function on.</param>
+        /// <returns>The <see cref="StaticBehaviour"/> component if it is found, <see cref="default"/> if not.</returns>
+        public static StaticBehaviour GetStaticStaticBehaviour(this GameObject gameObject) => gameObject.IsStatic() ? gameObject.GetComponent<StaticBehaviour>() : default;
     }
 }
