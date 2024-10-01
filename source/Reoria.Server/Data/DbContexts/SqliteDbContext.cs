a80308a42fd988cc9367dfa24397e1701dd9863f
XerShade <xershade.ca@gmail.com>
2024-10-01 07:11:45 -0400

    Added DbContext support.

---
+++ source/Reoria.Server/Data/DbContexts/SqliteDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Reoria.Engine;
+using Reoria.Game.DbContexts;
+
+namespace Reoria.Server.Data.DbContexts;
+
+public class SqliteDbContext : GameDbContext
+{
+    private readonly IConfigurationRoot configuration;
+
+    public SqliteDbContext() => this.configuration = new EngineServiceProvider().Configuration;
+    public SqliteDbContext(IConfigurationRoot configuration) => this.configuration = configuration;
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        _ = optionsBuilder.UseSqlite(this.configuration.GetConnectionString("GameDataConnectionString"));
+
+        base.OnConfiguring(optionsBuilder);
+    }
+}
--- source/Reoria.Server/Engine/ServerServiceProvider.cs
+++ source/Reoria.Server/Engine/ServerServiceProvider.cs
@@ -1,9 +1,15 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Reoria.Engine;
+using Reoria.Game.DbContexts;
+using Reoria.Server.Data.DbContexts;
 
 namespace Reoria.Server.Engine;
 
 public class ServerServiceProvider : EngineServiceProvider
 {
-    public ServerServiceProvider() : base() => this.serviceCollection.AddSingleton<ServerNetEventListener>();
+    public ServerServiceProvider() : base()
+    {
+        _ = this.serviceCollection.AddSingleton<ServerNetEventListener>();
+        _ = this.serviceCollection.AddDbContext<GameDbContext, SqliteDbContext>();
+    }
 }
